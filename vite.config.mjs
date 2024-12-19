2155c071574538187b6964289856127a8fbe471b
arjun.khunt <arjun.khunt@matrixcomsec.com>
2024-12-19 11:09:40 +0530

    Minimal PWA (installation, offline support)

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,70 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   build: {
     outDir: 'build',
+    sourcemap: 'hidden'
   },
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    devOptions: {
+      enabled: true
+    },
+
+    strategies: 'generateSW',
+    registerType: 'prompt',
+    injectRegister: 'auto',
+
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+
+    manifest: {
+      id: '/',
+      name: 'React Weather App',
+      short_name: 'ReactWeather',
+      description: 'A beautiful weather app',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ],
+      screenshots: [
+        {
+          "src": "reactWeatherSS.png",
+          "sizes": "1234×1072",
+          "type": "image/png",
+          "form_factor": "wide",
+          "label": "Home Page SS"
+        }
+      ],
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      cleanupOutdatedCaches: true,
+      sourcemap: true
+    }
+  })
+  ],
 });
